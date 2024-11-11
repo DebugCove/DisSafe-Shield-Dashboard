8e4cd5624b22059d7d242a360fce82b17e8a4d68
LuisGomes18 <luiscgomes0@gmail.com>
2024-11-11 00:04:42 +0000

    Addition of the dashboard route and removal of the rturn the home and addition of the CRSF in the discord connection

--- test/discord_login/app.py
+++ test/discord_login/app.py
@@ -1,10 +1,13 @@
 from os import getenv, urandom
 from flask import Flask, redirect, url_for, session, render_template
+from flask_session import Session
 from authlib.integrations.flask_client import OAuth
 
 
 app = Flask(__name__)
 app.secret_key = urandom(24)
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
 
 
 oauth = OAuth(app)
@@ -17,17 +20,12 @@ discord = oauth.register(
     access_token_url='https://discord.com/api/oauth2/token',
     authorize_url='https://discord.com/api/oauth2/authorize',
     api_base_url='https://discord.com/api/',
-    client_kwargs={
-        'scope': 'identify',
-    },
+    client_kwargs={'scope': 'identify', 'state': True},
 )
 
 
 @app.route('/')
 def home():
-    user = session.get('user')
-    if user:
-        return render_template('index.html', user=user)
     return render_template('index.html')
 
 
@@ -41,7 +39,16 @@ def callback():
     token = discord.authorize_access_token()
     user = discord.get('users/@me').json()
     session['user'] = user
-    return redirect('/')
+    return redirect('/dashboard')
+
+
+@app.route('/dashboard')
+def dashboard():
+    user = session.get('user')
+    if not user:
+        return redirect('login')
+
+    return render_template('dashboard.html', user=user)
 
 
 @app.route('/logout')
